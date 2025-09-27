40cad73bcbda4d9ab6ba0192db86cd3548c0da78
tailhaver <taggie@yip.cat>
2025-09-27 12:51:26 -0400

    pauses work yay

--- src/textarea.py
+++ src/textarea.py
@@ -1,6 +1,6 @@
 import pygame
 from .text import Line
-from .utils import load_lines_from_txt
+from .utils import load_lines_from_file
 
 class TextArea:
     def __init__(self, window, width: int):
@@ -8,20 +8,20 @@ class TextArea:
         self.surface = pygame.Surface((width, 0), flags=pygame.SRCALPHA)
         self.font_size = 20
         self.line_height = 20
-        self.lines = load_lines_from_txt("lines.txt")
+        self.lines = load_lines_from_file("lines.json")
         self.total_height = 0
     
     def fill(self, color) -> None:
         self.window.fill(color)
 
     def draw_most_recent_line(self) -> None:
-        lines = list(filter(lambda x: x[1].drawing, enumerate(self.lines)))
+        lines = list(filter(lambda x: x[1].drawing != False, enumerate(self.lines)))
         if len(lines) == 0: return
-        line = lines[0][1]
+        line: Line = lines[0][1]
         index = lines[0][0]
         if self.surface.height < self.total_height + line.surface.height:
             self.extend_surf(line.surface.height)
-        if line.drawing:
+        if line.drawing != False:
             line.draw_next()
             self.fill((255, 255, 255))
             self.window.blit(line.surface, (0, index*self.line_height))
