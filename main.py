f78a66eb9d4277f30335356673eab20c7c6d0121
tailhaver <taggie@yip.cat>
2025-09-27 12:00:15 -0400

    push font render

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import pygame
 pygame.init()
 pygame.font.init()
 
-from src.window import
+from src.window import Window
+
+window = Window((640, 480))
+window.fill((255, 255, 255))
+window.draw_lines()
+window.update()
 
 while True:
     for event in pygame.event.get():
--- src/window.py
+++ src/window.py
@@ -1,16 +1,26 @@
 import pygame
 
-font = pygame.font.SysFont("clangen.ttf", 20)
 
 class Window:
     def __init__(self, size: tuple[int, int]) -> None:
-        self.window = pygame.display.set_mode((640, 480), flags=pygame.SRCALPHA)
-        self.lines = []
+        self.window = pygame.display.set_mode(size, flags=pygame.SRCALPHA)
+        self.font_size = 20
+        self.line_height = 20
+        self.font = pygame.font.Font("clangen.ttf", self.font_size)
+        self.lines = [
+            "test 1",
+            "test 2"
+        ]
     
     def fill(self, color) -> None:
         self.window.fill(color)
+
+    def update(self) -> None:
+        pygame.display.update()
     
     def draw_lines(self) -> None:
+        y = 0
         for line in self.lines:
-            text = font.render(line)
-            ...
+            line = self.font.render(line, True, (0, 0, 0))
+            self.window.blit(line, (0, y))
+            y += self.line_height
